dffd18dc1bfaabd88f2ba8a639d4050f77075e60
Jaap Oosterbroek <jaap.oosterbroek@gmail.com>
2020-12-27 09:51:47 -0300

    work on repeated measures anova

--- example/anova_repeated_measures.py
+++ example/anova_repeated_measures.py
@@ -19,24 +19,48 @@ context = tc.get_context_default()
 sample_0 = [8, 11, 9, 16, 24]
 sample_1 = [2, 1, 12, 11, 19]
 sample_2 = [-2, 0, 6, 2 , 11]
- 
+
+sample_0_encoded = ts.ckks_vector(context, sample_0)
+sample_1_encoded = ts.ckks_vector(context, sample_1)
+sample_2_encoded = ts.ckks_vector(context, sample_2)
+
+list_sample = []
+list_sample.append(sample_0)
+list_sample.append(sample_1)
+list_sample.append(sample_2)
+
+list_sample_encoded = []
+list_sample_encoded.append(sample_0_encoded)
+list_sample_encoded.append(sample_1_encoded)
+list_sample_encoded.append(sample_2_encoded)
+
 # 3 done by the agregator
-statistic_encoded = tt.encode_anova_one_way(context, list_sample_encoded)
+# statistic_encoded = tt.encode_anova_repeated_measures(context, list_sample_encoded)
 
-# 4 done by the key holder
-f_statistic, p_value = tt.decode_anova_one_way(context, statistic_encoded)
+# # 4 done by the key holder
+# f_statistic, p_value = tt.decode_anova_one_way(context, statistic_encoded)
 
 
-# p value should be between about 0.996 and 0.997
+# # p value should be between about 0.996 and 0.997
+# print('via tensealstat')
+# print('f_statistic: ' + str(f_statistic))
+# print('p_value: ' + str(p_value))
+
+statistic_encoded = ttt.encode_anova_repeated_measures(context, list_sample)
+f_statistic_sample, f_statistic_measurement, p_value_sample, p_value_measurement = ttt.decode_anova_repeated_measures(context, statistic_encoded)
+# Assert.IsTrue(0.995 < p_values.Item1);
+# Assert.IsTrue(p_values.Item1 < 0.996);
+# Assert.IsTrue(0.999 < p_values.Item2);
+# Assert.IsTrue(p_values.Item2 < 1.000);
 print('via tensealstat')
-print('f_statistic: ' + str(f_statistic))
-print('p_value: ' + str(p_value))
+print('f_statistic sample:      ' + str(f_statistic_sample))
+print('f_statistic measurement: ' + str(f_statistic_measurement))
+print('p_value sample:          ' + str(p_value_sample))
+print('p_value measurement:     ' + str(p_value_measurement))
 
-statistic_encoded = ttt.encode_anova_one_way(context, list_sample_encoded)
-f_statistic, p_value = ttT.decode_anova_one_way(context, statistic_encoded)
-
-f_statistic, p_value = stats.f_oneway(sample_0, sample_1, sample_2, sample_3)
-print('')
-print('via scipy')
-print('f_statistic: ' + str(f_statistic))
-print('p_value: ' + str(1 - (p_value / 2)))
+#does not not exist in scipy
+# f_statistic, p_value = stats.f_oneway(sample_0, sample_1, sample_2)
+# print('')
+# print('via scipy')
+# print('f_statistic: ' + str(f_statistic))
+# print('p_value: ' + str(1 - (p_value / 2)))
--- tensealstat/tools_statistic_test.py
+++ tensealstat/tools_statistic_test.py
@@ -18,6 +18,10 @@ class ToolsStatisticTest:
         return scalar
 
     @staticmethod
+    def encode_scalar_inv(context, scalar):
+        return 1/scalar
+
+    @staticmethod
     def inv_0t2(context, x, d=7):
         return 1/x
 
@@ -29,7 +33,6 @@ class ToolsStatisticTest:
     def sqr(context, x):
         return x * x
 
-    
     @staticmethod
     def count_all(context, list_sample):
         count = 0 
@@ -37,19 +40,42 @@ class ToolsStatisticTest:
             count += len(sample)
         return count
     
-
     @staticmethod
     def sum(context, sample):
         return np.sum(sample)
 
-        
     @staticmethod
     def sum_all(context, list_sample):
         all = 0
         for sample in list_sample:
             all += np.sum(sample)
         return all
-    
+
+    @staticmethod
+    def sum_sample(context, list_sample):
+        list_sum_sample = []
+        for sample in list_sample:
+            list_sum_sample.append(ToolsStatisticTest.sum(context, sample))
+        return list_sum_sample
+
+
+    @staticmethod
+    def sum_measurement(context, list_sample):
+        list_sum_measurement = []
+        for index_measurement in range(len(list_sample[0])):
+            list_sum_measurement.append(ToolsStatisticTest.encode_scalar(context, 0))
+            for sample in list_sample:
+                list_sum_measurement[index_measurement] += sample[index_measurement]
+        return list_sum_measurement
+
+    @staticmethod
+    def sum_squared_all(context, list_sample):
+        sum_squared_all = ToolsStatisticTest.encode_scalar(context, 0)
+        for sample in list_sample:
+            for measurement in sample:
+                sum_squared_all += ToolsStatisticTest.sqr(context, measurement)
+        return sum_squared_all
+
     @staticmethod
     def mean(context, sample):
         return np.mean(sample)
@@ -57,8 +83,6 @@ class ToolsStatisticTest:
 
     @staticmethod
     def mean_all(context, list_sample):
-        sum_all = ToolsStatisticTest.sum_all(context, list_sample)
-        count_all =ToolsStatisticTest.count_all(context, list_sample)
         return ToolsStatisticTest.sum_all(context, list_sample) / ToolsStatisticTest.count_all(context, list_sample)
 
     @staticmethod
@@ -152,11 +176,6 @@ class ToolsStatisticTest:
 
     @staticmethod
     def encode_anova_one_way(context, list_sample):
-        size = len(list_sample[0])
-        for sample in list_sample:
-            if len(sample) != size:
-                raise Exception('Samples should be of equal size')
-
         count_sample = len(list_sample)
         count_total = ToolsStatisticTest.count_all(context, list_sample)
         mean_total = ToolsStatisticTest.mean_all(context, list_sample)
@@ -173,7 +192,6 @@ class ToolsStatisticTest:
         for sample, sample_mean in zip(list_sample, list_sample_mean):
             for measurement in sample:
                 sse += ToolsStatisticTest.sqr(context, measurement - sample_mean)
-        #FTransform
 
         degrees_of_freedom_0 = (count_sample - 1.0)
         degrees_of_freedom_1 = (count_total - count_sample)
@@ -181,8 +199,6 @@ class ToolsStatisticTest:
         total_variance = sse / degrees_of_freedom_1
 
 
-        
-        
         #TODO these all need to get pickled properly or jsonsed or something so they can be moved over http
         statistic_encoded = {}
         statistic_encoded['type_statistic'] = 'anova_one_way'
@@ -202,9 +218,86 @@ class ToolsStatisticTest:
         summed_variance = statistic_encoded['summed_variance'] 
         total_variance = statistic_encoded['total_variance'] 
 
+        #F-transform
+
         f_statistic = summed_variance / total_variance
         p_value = f.cdf(f_statistic, degrees_of_freedom_0, degrees_of_freedom_1)
         return f_statistic, p_value
 
 
   
+
+  
+    @staticmethod
+    def encode_anova_repeated_measures(context, list_sample):
+        count_sample = len(list_sample)
+        count_measurement = len(list_sample[0])
+        for sample in list_sample:
+            if len(sample) != count_measurement:
+                raise Exception('Samples should be of equal size')
+
+        count_total = count_sample * count_measurement
+        total_mean = ToolsStatisticTest.mean_all(context, list_sample)
+        sum_squared_all = ToolsStatisticTest.sum_squared_all(context, list_sample)
+        list_sum_sample = ToolsStatisticTest.sum_sample(context, list_sample)
+        list_sum_measurement = ToolsStatisticTest.sum_measurement(context, list_sample)
+
+        # compute C
+        c = ToolsStatisticTest.sqr(context, total_mean * ToolsStatisticTest.encode_scalar(context, count_total))
+        c *= ToolsStatisticTest.encode_scalar_inv(context, count_total)
+
+        sstot = sum_squared_all - c
+
+        ssb = 0
+        for sum_measurement in list_sum_measurement:
+            ssb += ToolsStatisticTest.sqr(context, sum_measurement) 
+        ssb *= ToolsStatisticTest.encode_scalar_inv(context, count_sample)
+        ssb -= c
+
+        sstr = 0.0
+        for sum_sample in list_sum_sample:
+            sstr += ToolsStatisticTest.sqr(context, sum_sample)
+        sstr *= ToolsStatisticTest.encode_scalar_inv(context, count_measurement)
+        sstr -= c
+
+        sse = sstot - ssb - sstr
+
+        degrees_of_freedom_0_sample      = (count_sample - 1.0)
+        degrees_of_freedom_0_measurement = (count_measurement - 1.0)
+        degrees_of_freedom_1             = degrees_of_freedom_0_sample * degrees_of_freedom_0_measurement;
+
+        total_variance = sse * ToolsStatisticTest.encode_scalar_inv(context, degrees_of_freedom_1)
+        summed_variance_sample =  sstr * ToolsStatisticTest.encode_scalar_inv(context, degrees_of_freedom_0_sample)
+        summed_variance_measurement = ssb * ToolsStatisticTest.encode_scalar_inv(context, degrees_of_freedom_0_measurement)
+
+        
+        #TODO these all need to get pickled properly or jsonsed or something so they can be moved over http
+        statistic_encoded = {}
+        statistic_encoded['type_statistic'] = 'anova_repeated_measures'
+        statistic_encoded['degrees_of_freedom_0_sample'] = degrees_of_freedom_0_sample
+        statistic_encoded['degrees_of_freedom_0_measurement'] = degrees_of_freedom_0_measurement
+        statistic_encoded['degrees_of_freedom_1'] = degrees_of_freedom_1
+        statistic_encoded['summed_variance_sample'] = summed_variance_sample
+        statistic_encoded['summed_variance_measurement'] = summed_variance_measurement
+        statistic_encoded['total_variance'] = total_variance
+        return statistic_encoded
+    
+    @staticmethod
+    def decode_anova_repeated_measures(context, statistic_encoded):
+        if not statistic_encoded['type_statistic'] == 'anova_repeated_measures':
+            raise Exception('Incorrect statistic')
+
+        degrees_of_freedom_0_sample = statistic_encoded['degrees_of_freedom_0_sample']
+        degrees_of_freedom_0_measurement = statistic_encoded['degrees_of_freedom_0_measurement']
+        degrees_of_freedom_1 = statistic_encoded['degrees_of_freedom_1']
+        summed_variance_sample = statistic_encoded['summed_variance_sample'] 
+        summed_variance_measurement = statistic_encoded['summed_variance_measurement'] 
+        total_variance = statistic_encoded['total_variance'] 
+
+        #F-transform
+
+        f_statistic_sample = summed_variance_sample / total_variance
+        f_statistic_measurement = summed_variance_measurement / total_variance
+        p_value_sample = f.cdf(f_statistic_sample, degrees_of_freedom_0_sample, degrees_of_freedom_1)
+        p_value_measurement = f.cdf(f_statistic_measurement, degrees_of_freedom_0_measurement, degrees_of_freedom_1)
+        return f_statistic_sample, f_statistic_measurement, p_value_sample, p_value_measurement
